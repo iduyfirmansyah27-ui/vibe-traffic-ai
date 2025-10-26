1016189ce608a2de0586dccc1b8d42187f666035
Yudi Firmansyah <iduyfirmansyah27@gmail.com>
2025-10-26 15:27:52 +0700

    vibetraffic

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'url';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     port: 3000,
-    strictPort: true,
-    open: true,
-    host: true
+    host: true,
+    open: true
   },
   build: {
     outDir: 'dist',
